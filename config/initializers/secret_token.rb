1c7d4f4a3853cfdd4b982053c7cf3ebfe61068b3
Thi Duong <dathi@tma.com.vn>
2015-04-06 18:23:26 +0700

    changes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -6,4 +6,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MyApp::Application.config.secret_token = ENV['SECRET_TOKEN']
+MyApp::Application.config.secret_token = ENV['SECRET_TOKEN'] || '3eb6db5a9026c547c72708438d496d942e976b252138db7e4e0ee5edd7539457d3ed0fa02ee5e7179420ce5290462018591adaf5f42adcf855da04877827def2'
+
